b1d6ef272326e66192d4dc376fbb78b1d0a20b03
StackCanary <danizmd8@gmail.com>
2024-09-24 13:25:34 +0300

    feat: make poker chips rotate to one direction only

--- src/components/PokerChip.jsx
+++ src/components/PokerChip.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./PokerChip.css";
 
 function PokerChip({ value, onClick, className }) {
+  const [rotation, setRotation] = useState(0);
+
   const handleClick = () => {
+    setRotation(rotation + 360);
     onClick(parseInt(value));
   };
 
   return (
-    <div onClick={handleClick} className={`chip h-24 w-24 ${className}`}>
+    <div
+      onClick={handleClick}
+      className={`chip h-24 w-24 ${className}`}
+      style={{ rotate: `${rotation}deg` }}
+    >
       {value}$
     </div>
   );
